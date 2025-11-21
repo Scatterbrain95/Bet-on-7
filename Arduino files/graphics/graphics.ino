f9783dd69b7f377c491f042b348e7e5216c13cb8
Scatterbrain95 <alirocky62008@gmail.com>
2025-11-21 15:50:57 +0100

    Update graphics.ino

--- Arduino files/graphics/graphics.ino
+++ Arduino files/graphics/graphics.ino
@@ -1,6 +1,7 @@
 #include <U8glib.h>
 #include <math.h> 
-
+#include "Adafruit_GFX.h"
+#include <MCUFRIEND_kbv.h>
 U8GLIB_SSD1306_128X64 u8g(U8G_I2C_OPT_NO_ACK);
 void setup() {
   // put your setup code here, to run once:
