632251df61b1ba678042fae2a2f68fbd2a9e4f9a
Scatterbrain95 <alirocky62008@gmail.com>
2025-11-21 15:30:13 +0100

    Create graphics.ino

---
+++ Arduino files/graphics/graphics.ino
@@ -0,0 +1,13 @@
+#include <U8glib.h>
+#include <math.h> 
+
+U8GLIB_SSD1306_128X64 u8g(U8G_I2C_OPT_NO_ACK);
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
